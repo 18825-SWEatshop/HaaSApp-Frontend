7e4032c0ad8ddeae8347666483ebb5e92a011a18
Brendan Early <mymindstorm@evermiss.net>
2025-11-04 16:48:10 -0600

    herokuify the frontend

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import viteReact from '@vitejs/plugin-react'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import viteReact from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   server: { port: 3000 },
   plugins: [
-    tsConfigPaths({ projects: ['./tsconfig.json'] }),
+    tsConfigPaths({ projects: ["./tsconfig.json"] }),
 
     // TanStack Start/Router FIRST
     tanstackStart({
-      customViteReactPlugin: true,   // ← add this
-      srcDirectory: 'src',
-      start: { entry: './start.tsx' },
-      server: { entry: './server.ts' },
+      srcDirectory: "src",
+      start: { entry: "./start.tsx" },
+      server: { entry: "./server.ts" },
       // tsr: { verboseFileRoutes: false }, // optional
     }),
 
     // React AFTER
     viteReact(),
   ],
-})
+});
